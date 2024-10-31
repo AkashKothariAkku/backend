103c0f10e211e2f8987a3de279aa8793ec02ad4d
akashkothari <akashkothari80@gmail.com>
2024-10-31 17:56:23 +0530

    coo

--- src/utils/jwtToken.js
+++ src/utils/jwtToken.js
@@ -8,9 +8,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 240 * 60 * 60 * 1000
     ),
-    httpOnly: false,
     secure: true,
-    sameSite: 'None'
   }
 
   res.status(statusCode).cookie('token', token, options).json({
